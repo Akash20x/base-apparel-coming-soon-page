684f9e40eb6ce7e13fa834082f4cf634c255fe00
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2022-07-20 22:12:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 let email = undefined;
 
-document.getElementById("submit").addEventListener('click', validation);
+document.querySelector('.submit').addEventListener('click', validation);
 
 function validation(e) {
     e.preventDefault();
 
-    email = document.getElementById("email").value;
+    email = document.querySelector('.email').value;
 
     const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (emailReg.test(email) === false) {
-        document.getElementById("error_text").classList.remove('hidden');
-        document.getElementById("error_icon").classList.remove('hidden');        
-        document.getElementById("email").classList.add('error')
+        document.querySelector('.error_text').classList.remove('hidden');
+        document.querySelector('.error_icon').classList.remove('hidden');        
+        document.querySelector('.email').classList.add('error')
     } else {
-        document.getElementById("error_text").classList.add('hidden');
-        document.getElementById("error_icon").classList.add('hidden');    
-        document.getElementById("email").classList.remove('error')
-        document.getElementById("email").value = "";
+        document.querySelector('.error_text').classList.add('hidden');
+        document.querySelector('.error_icon').classList.add('hidden');    
+        document.querySelector('.email').classList.remove('error')
+        document.querySelector('.email').value = "";
     }
    
 }
